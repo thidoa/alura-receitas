ecfccd12a334abd2c13f2751d79cc783975f72ca
thidoa <thiagovini200@gmail.com>
2022-12-14 22:23:25 -0300

    Adicionei o mecanismo de busca da receita

--- receitas/urls.py
+++ receitas/urls.py
@@ -5,5 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('<int:receita_id>', views.receita, name="receita")
+    path('<int:receita_id>', views.receita, name="receita"),
+    path('buscar', views.buscar, name="buscar"),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- receitas/views.py
+++ receitas/views.py
@@ -17,3 +17,17 @@ def receita(request, receita_id):
         'receita': receita
     }
     return render(request, 'receita.html', context)
+
+def buscar(request):
+    lista_receitas = Receita.objects.order_by('-data_receita').filter(publicada=True)
+
+    if 'buscar' in request.GET:
+        nome_a_buscar = request.GET['buscar']
+        if buscar:
+            lista_receitas = lista_receitas.filter(nome_receita__icontains=nome_a_buscar)
+
+    dados = {
+        'receitas': lista_receitas,
+    }
+
+    return render (request, 'buscar.html', dados)

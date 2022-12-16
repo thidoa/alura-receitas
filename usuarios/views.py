96b0b8fecb4a7e33a31a3a9c1e8d0612a4ec14bf
thidoa <thiagovini200@gmail.com>
2022-12-16 09:28:50 -0300

    Adicionei as funcionalidades de logar e logout do usuario

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib import auth
 
 def cadastro(request):
 
@@ -29,10 +30,27 @@ def cadastro(request):
         return render(request, 'usuarios/cadastro.html')
 
 def login(request):
+
+    if request.method == 'POST':
+        email = request.POST['email']
+        senha = request.POST['senha']
+        if email == '' or senha == '':
+            print('Os campos email e senha não podem ficar em branco')
+            return redirect('login')
+        if User.objects.filter(email=email).exists():
+            nome = User.objects.filter(email=email).values_list("username", flat=True).get()
+            user = auth.authenticate(request, username=nome, password=senha)
+            if user is not None:
+                auth.login(request, user)
+                return redirect('dashboard')
     return render(request, 'usuarios/login.html')
 
 def logout(request):
-    pass
+    auth.logout(request)
+    return redirect('index')
 
 def dashboard(request):
-    pass
+    if request.user.is_authenticated:
+        return render(request, 'usuarios/dashboard.html')
+    else:
+        return redirect('index')
